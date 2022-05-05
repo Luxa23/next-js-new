f4dbf76b8c21522ff15b0c98c39f5d7a7e128703
Anna Rolfes <anna-rolfes@gmx.de>
2022-05-05 12:35:11 +0200

    Added styled components basics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
 
 export default MyApp;
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
+
+const theme = {
+  colors: {
+    primary: "#0070f3",
+  },
+};
